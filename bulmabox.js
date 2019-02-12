48e738628f57074effa801a84dd3a69e695225de
newms34 <newms3450@gmail.com>
2019-02-12 16:47:36 -0500

    added min version. Added ability to prevent callback from closing for custom diags

--- bulmabox.js
+++ bulmabox.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var bulmabox = {
+const bulmabox = {
     currParams: null
 };
 
@@ -51,20 +51,20 @@ bulmabox.alert = function (a, b, c) {
     if (!bulmabox.params.cb) {
         bulmabox.params.cb = function () {};
     }
-    var btns = '<button class=\'button is-info\' onclick=\'bulmabox.runCb(bulmabox.params.cb,true)\'>Okay</button>';
+    const btns = '<button class=\'button is-info\' onclick=\'bulmabox.runCb(bulmabox.params.cb,true)\'>Okay</button>';
     bulmabox.dialog(bulmabox.params.tt, bulmabox.params.ms, btns);
 };
 
 bulmabox.confirm = function (a, b, c) {
     bulmabox.params = bulmabox.sortParams(a, b, c, true);
-    var btns = '<button class=\'button is-success\' onclick=\'bulmabox.runCb(bulmabox.params.cb,true)\'>Okay</button><button class=\'button is-danger\' onclick=\'bulmabox.runCb(bulmabox.params.cb,false)\'>Cancel</button>';
+    const btns = '<button class=\'button is-success\' onclick=\'bulmabox.runCb(bulmabox.params.cb,true)\'>Okay</button><button class=\'button is-danger\' onclick=\'bulmabox.runCb(bulmabox.params.cb,false)\'>Cancel</button>';
     bulmabox.dialog(bulmabox.params.tt, bulmabox.params.ms, btns);
 };
 
 bulmabox.prompt = function (a, b, c) {
     bulmabox.params = bulmabox.sortParams(a, b, c, true);
     bulmabox.params.ms += '<br>\n    <div class=\'field\'>\n        <div class=\'control\'>\n            <input type="text" id="bulmabox-diag-txt" class=\'input\'>\n        </div>\n    </div>';
-    var btns = '<button class=\'button is-success\' onclick=\'bulmabox.runCb(bulmabox.params.cb,document.querySelector("#bulmabox-diag-txt").value)\'>Okay</button><button class=\'button is-danger\' onclick=\'bulmabox.runCb(bulmabox.params.cb,false)\'>Cancel</button>';
+    const btns = '<button class=\'button is-success\' onclick=\'bulmabox.runCb(bulmabox.params.cb,document.querySelector("#bulmabox-diag-txt").value)\'>Okay</button><button class=\'button is-danger\' onclick=\'bulmabox.runCb(bulmabox.params.cb,false)\'>Cancel</button>';
     bulmabox.dialog(bulmabox.params.tt, bulmabox.params.ms, btns);
 };
 
@@ -73,20 +73,21 @@ bulmabox.custom = function (a, b, c, d) {
     if (!d) {
         d = '<button class=\'button is-info\' onclick=\'bulmabox.runCb(bulmabox.params.cb,true)\'>Okay</button>';
     }
-    var btns = d;
+    const btns = d;
     bulmabox.dialog(bulmabox.params.tt, bulmabox.params.ms, btns);
 };
 bulmabox.kill = function (id) {
-    var el = document.querySelector('#' + id);
+    const el = document.querySelector('#' + id);
     el.parentNode.removeChild(el);
 };
-bulmabox.runCb = function (cb, data) {
+bulmabox.runCb = function (cb, data,keepAlive) {
     cb(data);
+    if(!keepAlive) return false;
     bulmabox.kill('bulmabox-diag');
 };
 
 bulmabox.dialog = function (tt, msg, btns) {
-    var diagDiv = document.createElement('div');
+    const diagDiv = document.createElement('div');
     diagDiv.className = 'modal is-active';
     diagDiv.id = 'bulmabox-diag';
     diagDiv.innerHTML = '\n<div class="modal-background" onclick=\'bulmabox.kill("' + diagDiv.id + '")\'></div>\n    <div class="modal-card">\n        <header class="modal-card-head">\n            <p class="modal-card-title">' + tt + '</p>\n            <button class="delete" aria-label="close" onclick=\'bulmabox.kill("' + diagDiv.id + '")\'></button>\n        </header>\n        <section class="modal-card-body">\n            ' + (msg || '') + '\n        </section>\n        <footer class="modal-card-foot">\n            ' + btns + '\n        </footer>\n    </div>\n\t';
