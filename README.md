d55038c73ae08e6865dc4f0f53edfa17dcbeef28
Newms34 <newms3450@gmail.com>
2019-12-31 18:35:53 -0500

    fonts fixed; better Readme

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 ## Contents:
  - [About](#about)
+ - [Updates](#updates)
  - [Usage](#usage)
  - [Arguments](#arguments)
  - [Errors](#errors)
@@ -16,6 +17,13 @@ Bulmabox is a bare-bones dialog box helper in the style of Bootstrap's Bootbox, 
 
 ----
 
+## Updates
+You may notice that bulmabox has recently been update to version 7.0.0. Despite the big semantic version change (6 to 7), there shouldn't be any *major* differences on your end. However, a lot of stuff in the machinery has been replaced/reorganized to make bulmabox more stream-lined. Stuff you *should* know:
+ - You can now specify fonts. See below under `Special Notes` --> `Options` for details.
+ - Button creation has been centralized. This *shouldn't* mean anything for you, but if anything breaks, please let me know via creating an issue!
+
+----
+
 ## Usage
 #### Installation
 Just download the index.js file and include it in your HTML with a `script` tag for now.
@@ -27,20 +35,38 @@ Right now, Bulmabox has four separate functions that you can call:
  - `bulmabox.confirm()`: Confirm. Requires a callback function.
  - `bulmabox.prompt()`: Prompt. Also requires a callback function.
  - `bulmabox.custom()`: Dialog with customizable body. 
+
 ---
+
 ## Arguments
-The arguments for the functions are as follows:
- * **Title** (string): The title of your dialog box. Required!
+The arguments for the bulmabox functions are listed below. Anything that isn't marked "required" is... not required. Surprise!
+ * **Title** (string): The title of your dialog box. *Required!*
  * **Message** (string): The message of your dialog box. Note: the *second* string provided will *always* be treated as the message. If you only provide one string, you'll just have a title and no message.
  * **Callback** (function): What happens after you click a button. See `Errors` below for which functions require callbacks.
- * **Buttons** (HTML): (only for Custom dialog. Optional). A bunch of Bulma-formatted buttons. See the note below about custom dialogs for important information!
- * **Options** (object): (optional) Generally, this controls the color and language (or custom text) of each button. See `Buttons` below.
+ * **Buttons** (HTML): (only for Custom dialog). A bunch of Bulma-formatted buttons. See the note below about custom dialogs for important information!
+ * **Options** (object): Generally, this controls "appearance" of each button. See section "Options" under `Special Notes` below.
+
 ---
+
 ## Errors
+ (that hopefully aren't *my* fault)
  - If you feed the functions less than one string, Bulmabox will throw an error. I can't make an alert if there's nothing to say!
  - If you use a Confirm or Prompt function and fail to provide a callback function, Bulmabox will throw an error. Both of these expect to *do* something with whatever you're confirming/prompting for, so we need a callback for that. Alert and Custom do not require callbacks.
+
 ---
+
 ## Special Notes:
+##### Options:
+The "options" object passed to the functions has a few options itself:
+* **Button Language**: `lang:"fr"`.This allows you to pick the language of the buttons, from 40-ish available languages. Please note that I only speak Latin and English, so I cannot guarantee the translations (which I stole from bootbox. Thanks guys!) are correct. 
+Also, note that the button language is per-dialog box. You cannot, for example, do `{okay:french,cancel:chinese}` or something; you'd have to specify the button text manually in that case (see `buttons` - `text` below)
+* **Okay and Cancel Options (`{okay:{//stuff}, cancel: {//stuff}}`)**: Passed as two separate objects, `okay` and `cancel`. Each of these has a bunch of properties:
+    * **Colors (`colors`):** Passed as a single object, `colors`. This object has two properties`fg` and `bg`, which are, as you'd guess, foreground and background. The colors themselves are either:
+        * Bulma class names (i.e., `is-success` for background, or `has-text-success` for foreground).
+        * Regular old color codes (i.e., `#fcb340` or `rgb(252,179,64)`)
+    * **Text (`txt`):** The text displayed on each button. Note that if you do this, it will take precedence over custom language settings. So doing `{lang:'fr',cancel:{txt:'Nah Im good!'}}` will create a dialog with a cancel button that says "Nah Im good", *not* "Annuler".
+    * **Font (`font`):** The default Bulma font is easy to read, but what if you don't want easy? What if you want your buttons in Klingon or Quenya? You can do that! Just pass a `font` property to any buttons you wanna re-style (e.g., `{okay:{font:'Comic Sans MS'}}`). Please note that this is fed directly to the CSS `font-family` property, so doing stuff like `'24pt italic Comic Sans MS'`... won't work.
+
 ##### Custom Dialogs:
  Since the custom dialog's buttons are themselves custom (you can do whatever you want with them), you'll need to manually run some functions if you choose to include said buttons:
  - `bulmabox.runCb(callback,data,[keepAlive])`: Basically, your "Okay" function. This itself has a few parameters:
@@ -49,24 +75,19 @@ The arguments for the functions are as follows:
     - **keepAlive** (boolean) will prevent the custom dialog box from closing on running this callback. If *not* truthy, this will run `bulmabox.kill`, thus closing the modal.
  - `bulmabox.kill('bulmabox-diag')` if you just want the button to close the dialog. You can also substitute another DOM id for `'bulmabox-diag'`, but that'll close whatever that is and *not* the bulmabox modal. Note that if you run `bulmabox.runCb` without a `keepAlive` parameter, that will in turn run this, this closing the dialog box.
 
-##### Options:
-The "options" object passed to the functions has a few options itself:
-* **Button Colors**: Passed as two separate objects, `okay` and `cancel`. Each of these has two separate properties, `fg` and `bg`, which are, as you'd guess, foreground and background. The colors themselves are either:
-    * Bulma class names (i.e., `is-success` for background, or `has-text-success` for foreground).
-    * Regular old color codes (i.e., `#fcb340` or `rgb(252,179,64)`)
-* **Button Language**: `lang:"fr"`.This allows you to pick the language of the buttons, from 40 available languages. Please note that I only speak Latin and English, so I cannot guarantee the translations (which I stole from bootbox. Thanks guys!).
-* **Button Text**: If, as part of the `okay` or `cancel` objects, you pass a `txt` parameter, that will be used to replace the `okay` or `cancel` button text as appropriate. Note that if you do this, it will take precedence over custom language settings. So doing `{lang:'fr',cancel:{txt:'Nah Im good!'}}` will create a dialog with a cancel button that says "Nah Im good", *not* "Annuler".
-* **Button Font (New!)**: The default Bulma font is easy to read, but what if you don't want easy? What if you want your buttons in Klingon or Quenya? You can do that! Just pass a `font` property to any buttons you wanna re-style (e.g., `{okay:{font:'Comic Sans MS'}}`). Please note that this is fed directly to the CSS `font-family` property, so doing stuff like '24pt italic Comic Sans MS'... probably won't work.
-
-#### Global Options (New-ish!):
-You can now set global options for four parameters:
+##### Global Options:
+You can set global options for four parameters:
  * The language: `bulmabox.opts({lang:'fr'})`
  * The header and footer background: `bulmabox.opts({hfBg:'red'})`
  * The main background: `bulmabox.opts({mainBg:'blue'})`
  * Centering the text in the header and footer: `bulmabox.opts({centr:true})`
 
- Finally, you can reset all of this by just running `bulmabox.opts({reset:true})`. Finally, you can override the global `window.alert()` method by running `bulmabox.opts({globalAlert:true})` 
+ You can reset all of this by just running `bulmabox.opts({reset:true})`. 
+ Finally, you can override the global `window.alert()` method by running `bulmabox.opts({globalAlert:true})`.
+ 
+ If you're wondering, similar constructions do *not* work for any of the other dialogs (i.e., `window.confirm=bulmabox.confirm`), since that would require essentially pausing the browser script engine (or worse, crashing it!), which even the WHATWG people say is a bad idea (see [this link](https://html.spec.whatwg.org/multipage/webappapis.html#pause) on "pausing" as part of the `window.confirm` spec).
 
+--- 
 ##### **_Examples:_**
 Dialog with an okay button of "Sic" and a cancel button of "Non"
 
@@ -83,13 +104,16 @@ Dialog with an okay button of "Got it!" and a cancel button of "İptal" (Turkish
         }
     }
 
-Dialog with an okay button with blue fg, `is-success` bg, and Latin buttons:
+Dialog with an okay button with blue fg, `is-success` bg, Comic Sans font (...sorry) and Latin buttons:
     
     op = {
         lang:'la',
         okay:{
-            bg:"is-success",
-            fg:"#009"
+            colors:{
+                bg:"is-success",
+                fg:"#009"
+            },
+            font:'Comic Sans MS'
         }
     }
 ----
--- bulmabox.js
+++ bulmabox.js
@@ -8,11 +8,10 @@ const bulmabox = {
         centr: false,
         mainBg: '#fff'
 
-    }
-},
-    fgColorOpts = ['has-text-white', 'has-text-black', 'has-text-light', 'has-text-dark', 'has-text-primary', 'has-text-link', 'has-text-info', 'has-text-success', 'has-text-warning', 'has-text-danger'],
-    bgColorOpts = ['is-white', 'is-black', 'is-light', 'is-dark', 'is-primary', 'is-link', 'is-info', 'is-success', 'is-warning', 'is-danger'],
-    allLangs = {
+    },
+    fgColorOpts:['has-text-white', 'has-text-black', 'has-text-light', 'has-text-dark', 'has-text-primary', 'has-text-link', 'has-text-info', 'has-text-success', 'has-text-warning', 'has-text-danger'],
+    bgColorOpts:['is-white', 'is-black', 'is-light', 'is-dark', 'is-primary', 'is-link', 'is-info', 'is-success', 'is-warning', 'is-danger'],
+    allLangs:{
         "ar": {
             "OK": "موافق",
             "CANCEL": "الغاء"
@@ -177,7 +176,8 @@ const bulmabox = {
             "OK": "OK",
             "CANCEL": "取消"
         }
-    };
+    }
+};
 
 //arg order: first string is title. second string (if present) is main 'body' message. function is callback
 
@@ -241,74 +241,101 @@ bulmabox.sortParams = function (a, b, c, d, e) {
     // console.log('P, needsCb', p, needsCb)
     return p;
 };
-bulmabox.getBtnData = (op, btnStr, type) => {
+bulmabox.getBtnData = (op, type) => {
     //this fn most importantly gets the appropriate language translation for each button 
     //secondly, it gives us the option to pick the button color (other than defaults)
     //finally, it gets font choice(s), if specified
     //text first
     //firstly, if we specify the custom text, use that.
-    console.log('GET BTN DATA', op, btnStr, type, bulmabox.config)
+    // console.log('GET BTN DATA', op, type, bulmabox.config)
+    /* Get btn data using options (op), type */
+    const btns = {
+        okay: `<button class='button is-success-REPL has-text-okay-REPL' onclick='bulmabox.runCb(bulmabox.params.cb,true)' style='cust-bg-style;cust-fg-style;cust-font-style;'>Okay</button>`,
+        cancel: null,
+        btnList: ['okay']
+    };
+    if (type == 'confirm' || type == 'prompt') {
+        btns.cancel = `<button class='button is-danger-REPL has-text-cancel-REPL' onclick='bulmabox.runCb(bulmabox.params.cb,false)' style='cust-bg-style;cust-fg-style;cust-font-style;'>Cancel</button>`;
+        btns.btnList.push('cancel');
+    }
+    if(type=='prompt'){
+        //prompt uses a text-input value instead of just "true" as its data
+        btns.okay = btns.okay.replace('bulmabox.runCb(bulmabox.params.cb,true)',`bulmabox.runCb(bulmabox.params.cb,document.querySelector("#bulmabox-diag-txt").value)`);
+    }else if(type=='alert'){
+        btns.okay = btns.okay.replace('is-success-REPL','is-info-REPL')
+    }
     let hasCustTxt = false;
     if (op.okay && op.okay.txt) {
-        btnStr = btnStr.replace('Okay', op.okay.txt)
+        btns.okay = btns.okay.replace('Okay', op.okay.txt)
         hasCustTxt = true;
     }
-    if (op.cancel && op.cancel.txt) {
-        btnStr = btnStr.replace('Cancel', op.cancel.txt)
+    if (op.cancel && btns.cancel && op.cancel.txt) {
+        btns.cancel = btns.cancel.replace('Cancel', op.cancel.txt)
         hasCustTxt = true;
     }
     //now if we dont have custom text, and we have a language:
-    if (!hasCustTxt && op.lang && allLangs[op.lang]) {
-        btnStr = btnStr.replace('Okay', allLangs[op.lang].OK).replace('Cancel', allLangs[op.lang].CANCEL)
+    if (!hasCustTxt && op.lang && bulmabox.allLangs[op.lang]) {
+        btns.okay = btns.okay.replace('Okay', bulmabox.allLangs[op.lang].OK)
+        if(!!btns.cancel){
+            btns.cancel.replace('Cancel', bulmabox.allLangs[op.lang].CANCEL)
+        }
     }
 
-    //now colors:
+    //now colors and fonts:
     //okay btn
-    if (op.okay && op.okay.colors) {
-        //foreground
-        const defCol = type == 'confirm' || type == 'prompt' ? 'success' : 'info'
-        if (op.okay.colors.fg && fgColorOpts.includes(op.okay.colors.fg)) {
-            //user specified a color option the fg color for Okay btn, and it's one of the bulma classes
-            btnStr = btnStr.replace(`has-text-okay-REPL`, op.okay.colors.fg)
-        } else if (op.okay.colors.fg) {
-            btnStr = btnStr.replace('cust-fg-style', `color:${op.okay.colors.fg}`)
+    
+    if (op.okay) {
+        //colors
+        if(op.okay.colors){
+            const defCol = type == 'confirm' || type == 'prompt' ? 'success' : 'info'
+            if (op.okay.colors.fg && bulmabox.fgColorOpts.includes(op.okay.colors.fg)) {
+                //user specified a color option the fg color for Okay btn, and it's one of the bulma classes
+                btns.okay = btns.okay.replace(`has-text-okay-REPL`, op.okay.colors.fg)
+            } else if (op.okay.colors.fg) {
+                btns.okay = btns.okay.replace('cust-fg-style', `color:${op.okay.colors.fg}`)
+            }
+            if (op.okay.colors.bg && bulmabox.bgColorOpts.includes(op.okay.colors.bg)) {
+                //user specified a color option the bg color for Okay btn, and it's one of the bulma classes
+                btns.okay = btns.okay.replace(`is-${defCol}-REPL`, op.okay.colors.bg)
+            } else if (op.okay.colors.bg) {
+                btns.okay = btns.okay.replace(`cust-bg-style`, `background:${op.okay.colors.bg}`)
+            }
         }
-        if (op.okay.colors.bg && bgColorOpts.includes(op.okay.colors.bg)) {
-            //user specified a color option the bg color for Okay btn, and it's one of the bulma classes
-            btnStr = btnStr.replace(`is-${defCol}-REPL`, op.okay.colors.bg)
-        } else if (op.okay.colors.bg) {
-            // console.log('replacing bg color!', op.okay.colors.bg)
-            btnStr = btnStr.replace(`cust-bg-style`, `background:${op.okay.colors.bg}`)
-        }
-    }
-    if (op.cancel && op.cancel.colors) {
-        //foreground
-        if (op.cancel.colors.fg && fgColorOpts.includes(op.cancel.colors.fg)) {
-            //user specified a color option the fg color for cancel btn, and it's one of the bulma classes
-            btnStr = btnStr.replace(`has-text-danger-REPL`, op.cancel.colors.fg)
-        } else if (op.cancel.colors.fg) {
-            btnStr = btnStr.replace('cust-fg-style', `color:${op.cancel.colors.fg}`)
-        }
-        if (op.cancel.colors.bg && bgColorOpts.includes(op.cancel.colors.bg)) {
-            //user specified a color option the bg color for cancel btn, and it's one of the bulma classes
-            btnStr = btnStr.replace(`is-danger-REPL`, op.cancel.colors.bg)
-        } else if (op.cancel.colors.bg) {
-            // console.log('replacing bg color!', op.cancel.colors.bg)
-            btnStr = btnStr.replace(`cust-bg-style`, `background:${op.cancel.colors.bg}`)
+        if (op.okay && op.okay.font) {
+            btns.okay = btns.okay.replace(`cust-font-style;`, `font-family:${op.okay.font}`);
         }
     }
-
-    //fonts
-    if(op.okay && op.okay.font){
-        console.log('FONT',op.okay,'BEFORE REPLACE',btnStr)
-        btnStr = btnStr.replace(`cust-font-style;`,`font-family:${op.okay.font}`);
+    
+    //cancel btn
+    if(!!btns.cancel){
+        if (op.cancel && op.cancel.colors) {
+            //foreground
+            if (op.cancel.colors.fg && bulmabox.fgColorOpts.includes(op.cancel.colors.fg)) {
+                //user specified a color option the fg color for cancel btn, and it's one of the bulma classes
+                btns.cancel = btns.cancel.replace(`has-text-danger-REPL`, op.cancel.colors.fg)
+            } else if (op.cancel.colors.fg) {
+                btns.cancel = btns.cancel.replace('cust-fg-style', `color:${op.cancel.colors.fg}`)
+            }
+            if (op.cancel.colors.bg && bulmabox.bgColorOpts.includes(op.cancel.colors.bg)) {
+                //user specified a color option the bg color for cancel btn, and it's one of the bulma classes
+                btns.cancel = btns.cancel.replace(`is-danger-REPL`, op.cancel.colors.bg)
+            } else if (op.cancel.colors.bg) {
+                // console.log('replacing bg color!', op.cancel.colors.bg)
+                btns.cancel = btns.cancel.replace(`cust-bg-style`, `background:${op.cancel.colors.bg}`)
+            }
+        }
+        if (op.cancel && op.cancel.font) {
+            btns.cancel = btns.cancel.replace(`cust-font-style;`, `font-family:${op.cancel.font}`);
+        }
     }
-    if(op.cancel && op.cancel.font){
-        btnStr = btnStr.replace(`cust-font-style;`,`font-family:${op.cancel.font}`);
-    }
+    
     //finally, get rid of any remaining "custom" placeholders
-    btnStr = btnStr.replace(/cust-fg-style;/g, '').replace(/cust-bg-style;/g, '').replace(/-REPL/g, '').replace(/has-text-okay/g, '').replace(/has-text-cancel/g, '')
-    console.log(op, btnStr)
+    let btnStr = '';
+    btns.btnList.forEach(bn=>{
+        btnStr += btns[bn].replace(/cust-fg-style;/g, '').replace(/cust-bg-style;/g, '').replace(/-REPL/g, '').replace(/has-text-okay/g, '').replace(/has-text-cancel/g, '');
+    })
+    // btnStr = btnStr.replace(/cust-fg-style;/g, '').replace(/cust-bg-style;/g, '').replace(/-REPL/g, '').replace(/has-text-okay/g, '').replace(/has-text-cancel/g, '')
+    // console.log(op, btnStr)
     return btnStr;
 }
 
@@ -317,28 +344,30 @@ bulmabox.alert = (a, b, c, e) => {
     if (!bulmabox.params.cb) {
         bulmabox.params.cb = function () { };
     }
-    const btns = bulmabox.getBtnData(bulmabox.params.op, `<button class='button is-info-REPL has-text-okay-REPL' onclick='bulmabox.runCb(bulmabox.params.cb,true)' style='cust-bg-style;cust-fg-style;cust-font-style;'>Okay</button>`, 'alert');
+    const btns = bulmabox.getBtnData(bulmabox.params.op, 'alert');
     bulmabox.dialog(bulmabox.params.tt, bulmabox.params.ms, btns);
 };
 
 bulmabox.confirm = (a, b, c, e) => {
     bulmabox.params = bulmabox.sortParams(a, b, c, true, e);
-    const btns = bulmabox.getBtnData(bulmabox.params.op, `<button class='button is-success-REPL has-text-okay-REPL' onclick='bulmabox.runCb(bulmabox.params.cb,true)' style='cust-bg-style;cust-fg-style;cust-font-style;'>Okay</button><button class='button is-danger-REPL has-text-cancel-REPL' onclick='bulmabox.runCb(bulmabox.params.cb,false)' style='cust-bg-style;cust-fg-style;cust-font-style;'>Cancel</button>`, 'confirm');
+    const btns = bulmabox.getBtnData(bulmabox.params.op, 'confirm');
     bulmabox.dialog(bulmabox.params.tt, bulmabox.params.ms, btns);
 };
 
 bulmabox.prompt = (a, b, c, e) => {
     bulmabox.params = bulmabox.sortParams(a, b, c, true, e);
     bulmabox.params.ms += `<br>\n    <div class='field'>\n        <div class='control'>\n            <input type="text" id="bulmabox-diag-txt" class='input'>\n        </div>\n    </div>`;
-    const btns = bulmabox.getBtnData(bulmabox.params.op, `<button class='button is-success-REPL has-text-okay-REPL' onclick='bulmabox.runCb(bulmabox.params.cb,document.querySelector("#bulmabox-diag-txt").value)' style='cust-bg-style;cust-fg-style;cust-font-style;'>Okay</button><button class='button is-danger-REPL has-text-cancel-REPL' onclick='bulmabox.runCb(bulmabox.params.cb,false)' style='cust-bg-style;cust-fg-style;cust-font-style;'>Cancel</button>`, 'prompt');
+    const btns = bulmabox.getBtnData(bulmabox.params.op, 'prompt');
     bulmabox.dialog(bulmabox.params.tt, bulmabox.params.ms, btns);
 };
 
 bulmabox.custom = (a, b, c, d, e) => {
     d = d || {};
+    // console.log('custom d param',d)
     bulmabox.params = bulmabox.sortParams(a, b, c, false, e);
     if (!d) {
-        d = bulmabox.getBtnData(bulmabox.params.op, `<button class='button is-info-REPL has-text-okay-REPL' onclick='bulmabox.runCb(bulmabox.params.cb,true)' style='cust-bg-style;cust-fg-style;cust-font-style;'>Okay</button>`, 'custom');
+        //no button(s) specified; create one
+        d = bulmabox.getBtnData(bulmabox.params.op, 'custom-default');
     }
     const btns = d;
     bulmabox.dialog(bulmabox.params.tt, bulmabox.params.ms, btns);
@@ -369,7 +398,7 @@ bulmabox.opts = (o) => {
     } else if (o && o.hfBg === null) {
         bulmabox.config.mainBg = '#fff';
     }
-    if (o.lang && Object.keys(allLangs).includes(o.lang)) {
+    if (o.lang && Object.keys(bulmabox.allLangs).includes(o.lang)) {
         bulmabox.config.lang = o.lang;
     }
     if (o.centr || o.centr === false) {
